47df9b8cdda0ee0226b945b3c9840cf46ce9897b
coolcat647 <b10302017@mail.ntust.edu.tw>
2019-07-19 04:01:13 +0800

    fix cuda version bug for RTX2060

--- Dockerfiles/docker_build.sh
+++ Dockerfiles/docker_build.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env sh
+COLOR_YELLOW='\033[0;33m'
+COLOR_NC='\033[0m'
 
-docker build -t argnctu/ros-caffe -f Dockerfile .
+if [ $# -gt 0 ]; then
+    if [ "$1" == "cuda8" ]; then
+        docker build -t argnctu/ros-caffe -f Dockerfile-cuda8 .
+    elif [ "$1" == "cuda10" ]; then
+        docker build -t argnctu/ros-caffe -f Dockerfile-cuda10 .
+    else
+        echo -e "Please specify which cuda version your GPU support."
+        echo -e "${COLOR_YELLOW}Usage: source docker_build.sh [cuda8 | cuda10]${COLOR_NC}"
+    fi
+else
+    echo -e "${COLOR_YELLOW}Usage: source docker_build.sh [cuda8 | cuda10]${COLOR_NC}"
+fi
